19aa674f2d55a41a33816d886cef628b354f40a6
wxy@jlwh.com <wxy@jlwh.com>
2022-08-06 10:11:56 +0800

    first commit

---
+++ contracts/SunswapV2Factory.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.10;
+
+import './interfaces/ISunswapV2Factory.sol';
+import './SunswapV2Pair.sol';
+
+contract SunswapV2Factory is ISunswapV2Factory {
+    address public feeTo;
+    address public feeToSetter;
+
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    constructor(address _feeToSetter) public {
+        feeToSetter = _feeToSetter;
+    }
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        require(tokenA != tokenB, 'SunswapV2: IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'SunswapV2: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'SunswapV2: PAIR_EXISTS');
+        // single check is sufficient
+        bytes memory bytecode = type(SunswapV2Pair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        ISunswapV2Pair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function setFeeTo(address _feeTo) external {
+        require(msg.sender == feeToSetter, 'SunswapV2: FORBIDDEN');
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, 'SunswapV2: FORBIDDEN');
+        feeToSetter = _feeToSetter;
+    }
+
+    function getPairHash() public pure returns (bytes32) {
+        return keccak256(type(SunswapV2Pair).creationCode);
+    }
+}
